10afd2896acb748626f409019671252da24f2eeb
DLLOPEZ <dllopezhe@gmail.com>
2023-03-28 17:13:46 -0500

    ACTUALIZACION

---
+++ Ejercicios_Varios/Java/Proyecto_Java/src/main/java/Ejecicios/Ejercicio_5.java
@@ -0,0 +1,31 @@
+package Ejecicios;
+
+import java.util.Scanner;
+
+public class Ejercicio_5 {
+/*
+Escribir un programa que pida al usuario dos números y muestre por
+pantalla su división. Si el divisor es cero el programa debe mostrar un error.
+ */
+    public static void main(String[] args) {
+
+        Scanner input1= new Scanner(System.in);
+        Scanner input2= new Scanner(System.in);
+        System.out.println("ingrese el dividendo valor: ");
+        float valor1= input1.nextFloat();
+        System.out.println("ingrese el divisor valor: ");
+        float valor2= input2.nextFloat();
+
+        float resultado = valor1/valor2;
+
+        if (valor2==0){
+            System.out.println("ERROR---> el divisor no puede ser cero");
+        }
+        else {
+            System.out.println("la division es: " +resultado);
+        }
+
+
+    }
+
+}
---
+++ Ejercicios_Varios/Java/Proyecto_Java/src/main/java/Ejecicios/JS/test.js
@@ -0,0 +1,19 @@
+/*let numero1 = prompt("Ingrese numero a dividir: ");
+let numero2 = prompt("Ingrese el numero el cual se va dividir: ");
+
+if (numero2 == 0) {
+  console.log("ERROR ---> NO PUEDE SER VALOR CERO");
+} else {
+  var resultado = numero1 / numero2;
+  console.log("El resultado de la division es " + resultado);
+}*/
+var key = "contraseña";
+var password = prompt("Ingrese su contraseña");
+
+if (key==password)
+{
+    console.log("La contraseña coincide")
+}
+else{
+    console.log("La contraseña No es igual")
+}
